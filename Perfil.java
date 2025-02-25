92cfbf1dff51a271bd395544ab8d18258dfdbf14
Luigi Angeli <luigi.angeli.francisco@gmail.com>
2025-02-24 21:28:44 -0300

    Create Perfil.java

---
+++ Perfil.java
@@ -0,0 +1,10 @@
+public class Perfil {
+    public static void main(String[] args) {
+        String nome = "Luigi Angeli Francisco";
+        int idade = 19;
+        String hobby = "Jogar jogos";
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Hobby: " + hobby);
+    }
+}
